d8cb972088bafdde28682a25d7d44d666e35d08a
ujasitalia <ujasitalia216@gmail.com>
2023-04-14 08:22:03 -0400

    get all jobs

--- backend/data/jobseeker.js
+++ backend/data/jobseeker.js
@@ -1,5 +1,23 @@
 const mongoCollections = require("../config/mongoCollections");
 const jobSeekers = mongoCollections.jobSeekers;
+const jobs = mongoCollections.jobs;
 const { ObjectId } = require("mongodb");
 
-module.export = {};
+const getAllJobs = async () => {
+  const jobsCollection = await jobs();
+  const allJobs = await jobsCollection.find(
+    {},
+    { projection: { applications: 0 } }
+  );
+
+  if (!allJobs) throw { status: "400", error: "Could not get all Jobs" };
+
+  for (let i = 0; i < allJobs.length; i++)
+    allJobs[i]._id = allJobs[i]._id.toString();
+
+  return allJobs;
+};
+
+module.export = {
+  getAllJobs,
+};
