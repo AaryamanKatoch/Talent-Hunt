9d9acc7acc60844dc37bce187ee0b697d8d60546
DhavanKanakia <dkanaki1@stevens.edu>
2023-04-28 15:16:00 -0400

    resolved conflicts

--- jobseeker/src/pages/Home.js
+++ jobseeker/src/pages/Home.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect,useContext} from 'react';
-import axios from 'axios';
-import Logout from './Logout';
-import {Link,useParams} from 'react-router-dom';
-import { styled } from '@mui/material/styles';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import Logout from "./Logout";
+import { Link, useParams } from "react-router-dom";
+import { styled } from "@mui/material/styles";
 import {
   Card,
   CardActionArea,
@@ -11,27 +11,27 @@ import {
   Grid,
   Typography,
   ButtonBase,
-  Button
-} from '@mui/material';
-import noImage from '../assets/css/download.jpeg';
+  Button,
+} from "@mui/material";
+import noImage from "../assets/css/download.jpeg";
 // import { AuthContext } from "../firebase/Auth";
 
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  maxWidth: "100%",
+  maxHeight: "100%",
 });
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
   const [searchData, setSearchData] = useState(undefined);
   const [jobsData, setJobsData] = useState(undefined);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [pageNo, setPageNo] = useState(1);
   const [b_disabled, setBDisable] = useState(false);
   let page_player = useParams().page;
-  let [hasError,setError] = useState(false);
+  let [hasError, setError] = useState(false);
   // const {currentUser} = useContext(AuthContext);
   let card = null;
   const regex = /(<([^>]+)>)/gi;
@@ -48,13 +48,16 @@ const Home = () => {
   }, [b_disabled]);
 
   useEffect(() => {
-    console.log('on load useeffect');
+    console.log("on load useeffect");
     // console.log(currentUser);
     // console.log(currentUser.email)
     // console.log(currentUser.displayName)
     async function fetchData() {
       try {
-        const {data} = await axios.get('http://localhost:3000/jobseeker/jobs',{params: { page: page_player }});
+        const { data } = await axios.get(
+          "http://localhost:3000/jobseeker/jobs",
+          { params: { page: page_player } }
+        );
         setJobsData(data);
         setLoading(false);
       } catch (e) {
@@ -62,21 +65,21 @@ const Home = () => {
       }
     }
     fetchData();
-  }, [page_player]); 
+  }, [page_player]);
 
   const buildCard = (job) => {
     return (
       <Grid item xs={10} sm={10} md={10} lg={10} xl={10} key={job.id}>
         <Card
           sx={{
-            width: '100%',
-            height: 'auto',
-            display: 'flex' ,
+            width: "100%",
+            height: "auto",
+            display: "flex",
             // maxHeight:500,
-            marginTop:5,
+            marginTop: 5,
             boxShadow:
-              '0 15px 30px rgba(0,0,0,0.30), 0 10px 8px rgba(0,0,0,0.22);',
-            textDecoration: 'none'
+              "0 15px 30px rgba(0,0,0,0.30), 0 10px 8px rgba(0,0,0,0.22);",
+            textDecoration: "none",
           }}
         >
           <CardMedia
@@ -86,40 +89,42 @@ const Home = () => {
             alt="Live from space album cover"
           />
           <CardActionArea>
-            <Link className='Link-for-eventcard' to={`/`}>
+            <Link className="Link-for-eventcard" to={`/jobDetails/${job._id}`}>
               <CardContent>
-              <Typography
+                <Typography
                   sx={{
-                    fontWeight: 'bold',
-                    height:'40px',
+                    fontWeight: "bold",
+                    height: "40px",
                   }}
-                  component='h3'
+                  component="h3"
                 >
                   Job title
                 </Typography>
                 <Typography
                   sx={{
-                    fontWeight: 'bold',
-                    height:'40px',
+                    fontWeight: "bold",
+                    height: "40px",
                   }}
-                  component='h3'
+                  component="h3"
                 >
-                  {job.description.replace(regex, '').substring(0, 35).toString()}
+                  {job.description
+                    .replace(regex, "")
+                    .substring(0, 35)
+                    .toString()}
                 </Typography>
               </CardContent>
             </Link>
           </CardActionArea>
-
         </Card>
       </Grid>
     );
   };
 
   card =
-  jobsData &&
-      jobsData.map((job) => {
-        return buildCard(job);
-      });
+    jobsData &&
+    jobsData.map((job) => {
+      return buildCard(job);
+    });
 
   if (loading) {
     return (
@@ -127,39 +132,67 @@ const Home = () => {
         <h2>Loading....</h2>
       </div>
     );
-  } else if(hasError){
-    return (<div><h2>has error</h2></div>);   
-  } else{
+  } else if (hasError) {
+    return (
+      <div>
+        <h2>has error</h2>
+      </div>
+    );
+  } else {
     return (
       // <div>
       //   <h2>Home</h2>
       //   <Logout/>
       // </div>
-            <div className='main-div'>
-              <div className='page-div makeCenter'>
-                  {page_player>1 && (<Link to={`/page/${Number(page_player) - 1}`}><Button variant="outlined" color="secondary" type="submit"  className='page_button' disabled={b_disabled} onClick={clickEvent}>previous</Button></Link>)}
-                  <h4 className='page_indicator'>{page_player}</h4>
-                  {page_player<49 && (<Link to={`/page/${Number(page_player) + 1}`}><Button variant="outlined" color="secondary" type="submit" className='page_button'  disabled={b_disabled} onClick={clickEvent}>next</Button></Link>)}
-                  <br></br>
-                  <br></br>
-              </div>
-                <Grid 
-                    container 
-                    spacing={1}
-                    sx={{
-                        //flexGrow: 1,
-                        //flexDirection: 'column'
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                    >
-                    {card}
-                </Grid>
-            </div>
+      <div className="main-div">
+        <div className="page-div makeCenter">
+          {page_player > 1 && (
+            <Link to={`/page/${Number(page_player) - 1}`}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                type="submit"
+                className="page_button"
+                disabled={b_disabled}
+                onClick={clickEvent}
+              >
+                previous
+              </Button>
+            </Link>
+          )}
+          <h4 className="page_indicator">{page_player}</h4>
+          {page_player < 49 && (
+            <Link to={`/page/${Number(page_player) + 1}`}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                type="submit"
+                className="page_button"
+                disabled={b_disabled}
+                onClick={clickEvent}
+              >
+                next
+              </Button>
+            </Link>
+          )}
+          <br></br>
+          <br></br>
+        </div>
+        <Grid
+          container
+          spacing={1}
+          sx={{
+            //flexGrow: 1,
+            //flexDirection: 'column'
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {card}
+        </Grid>
+      </div>
     );
   }
+};
 
-  
-}
-
-export default Home
+export default Home;
