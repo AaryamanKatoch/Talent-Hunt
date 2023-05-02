ab9c60ad77b84893b418ec3f0fdf2a719b2d3a1d
ujasitalia <ujasitalia216@gmail.com>
2023-05-02 12:17:37 -0400

    redis for jobseeker applications

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ const app = express();
 const cors = require('cors');
 const session = require('express-session');
 const configRoutes = require('./routes');
+const redis = require("redis");
+const client = redis.createClient();
 const connection = require('./config/mongoConnection');
 // const http = require("http");
 const data = require('./data');
 const jobs=data.jobs;
+client.connect().then(() => {});
 
 app.use(express.json({limit: '50mb'}));
 app.use(cors());
@@ -18,6 +21,17 @@ app.use(
       saveUninitialized: true
   })
 );
+
+app.use("/jobseeker/HistoryOfApplications", async (req, res, next) => {
+  if(req.method === 'GET'){
+    let exists = await client.exists("jobSeekerApplications");
+    if(exists){
+      let applications = await client.get("jobSeekerApplications");
+      applications = JSON.parse(applications);
+      return res.status(200).json(applications);
+    }
+  }
+}
 
 // const main=async()=>{
 //   const db = await connection.dbConnection();
